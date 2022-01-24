9fdf05aa581f627f2a35f2b46934f706ed21b383
EA Aguilar <50111632+EdAlexAguilar@users.noreply.github.com>
2022-01-24 23:56:49 +0100

    refactored code base

--- GeometricControl/pure_pursuit.py
+++ GeometricControl/pure_pursuit.py
@@ -5,60 +5,68 @@ class PurePursuit():
     """
     Waypoints are ordered (carla.Waypoint objects)
     """
-    def __init__(self, vehicle, waypoints, world, lookahead=50):
+    def __init__(self, vehicle, waypoints, world, kpp=5, target_speed_kph=45):
         self.vehicle = vehicle
         self.waypoints = waypoints # list of Vector3D
         self.world = world # carla.world
-        self.lookahead = lookahead
-        self.Kpp = 5
+        self.kpp = kpp
+        self.target_speed_kph = target_speed_kph
         self.half_length = vehicle.bounding_box.extent.x  - 0.5 # half the vehicle's length correction to put in axle
         self.draw_waypoints()
 
     def draw_waypoints(self):
         for i in range(len(self.waypoints)-1):
-            self.world.debug.draw_line(self.waypoints[i], self.waypoints[i+1], color=carla.Color(0,100,0), life_time=0)
+            self.world.debug.draw_line(self.waypoints[i], self.waypoints[i+1], color=carla.Color(0,10,0), life_time=0)
 
     def append_waypoints(self, new_waypoints):
         self.waypoints = self.waypoints + new_waypoints
 
-    def update(self):
+    def longitudinal_control(self):
+        # ridiculous speed controller
         speed = self.vehicle.get_velocity()
         speed = speed.length()
-        # lookahead = self.lookahead # Naive Version
-        lookahead = self.Kpp*speed  # Updated version with speed dependence
-        control_command = carla.VehicleControl()
-        if speed<(50/3.6):
-            # ridiculous speed controller
-            control_command.throttle = 0.6
+        if speed < (self.target_speed_kph):
+            throttle = 0.6
         else:
-            control_command.throttle = 0.4
+            throttle = 0.4
+        return throttle
+
+    def lateral_control(self):
+        speed = self.vehicle.get_velocity()
+        speed = speed.length()
+        lookahead = self.kpp * speed
+
         transform = self.vehicle.get_transform()
         location = transform.location
         heading = transform.rotation.get_forward_vector()
-        rear_axle_loc = location - self.half_length*heading
+        rear_axle_loc = location - self.half_length * heading
+
         distances = np.array([(rear_axle_loc.distance(w) - lookahead) for w in self.waypoints])
         target_id = np.abs(distances).argmin()
         self.waypoints = self.waypoints[target_id:]
         v_target = self.waypoints[0] - rear_axle_loc
         v_target_length = v_target.length()
-        # assert abs(v_target_length - self.lookahead) < 8 , "Target Waypoint is not at desired lookahead dist"
+
         v_target_unitary = v_target.make_unit_vector()
         sin_alpha = v_target_unitary.cross(heading)
         sign_sin_alpha = np.sign(sin_alpha.dot(carla.Vector3D(z=1)))
-        sin_alpha = sin_alpha.length()*sign_sin_alpha
-        delta = - np.arctan(4*self.half_length*sin_alpha / v_target_length)
-        delta *= 2/np.pi
-        control_command.steer = delta
-        # control_command.steer = 0.5 * (2*np.random.rand() - 1)
-        self.vehicle.apply_control(control_command)
+        sin_alpha = sin_alpha.length() * sign_sin_alpha
+        delta = - np.arctan(4 * self.half_length * sin_alpha / v_target_length)
+        delta *= 2 / np.pi
 
         """
         Helpful Drawing of Lines
         """
         higher_rear_axle = rear_axle_loc
         higher_rear_axle.z += 0.4
-        self.world.debug.draw_arrow(higher_rear_axle, self.waypoints[0], thickness=0.2, color=carla.Color(0,0,50), life_time=0.08)
-        self.world.debug.draw_point(rear_axle_loc, size=0.05, color=carla.Color(0,0,0,50), life_time=0)
+        self.world.debug.draw_arrow(higher_rear_axle, self.waypoints[0],
+                                    thickness=0.2, color=carla.Color(0, 0, 50), life_time=0.08)
+        self.world.debug.draw_point(rear_axle_loc,
+                                    size=0.05, color=carla.Color(0, 50, 50, 50), life_time=0)
+        return delta
 
-
-        pass
+    def update(self):
+        control_command = carla.VehicleControl()
+        control_command.throttle = self.longitudinal_control()
+        control_command.steer = self.lateral_control()
+        self.vehicle.apply_control(control_command)
---
+++ test_controller.py
@@ -0,0 +1,80 @@
+import carla
+import numpy as np
+import GeometricControl.pure_pursuit as pp
+
+client = carla.Client('localhost', 2000)
+world = client.load_world('Town04')
+world.set_weather(carla.WeatherParameters().CloudySunset)
+settings = world.get_settings()
+settings.synchronous_mode = True
+settings.fixed_delta_seconds = 0.05
+world.apply_settings(settings)
+carla_map = world.get_map()
+
+def create_location_waypoints(road_list, lane=4):
+    """
+    :param road_list: road_id (int) list
+    """
+    navigation_waypoints = [w.transform.location for w in carla_map.generate_waypoints(WAYPOINT_DIST)
+                            if w.road_id==road_list[0] and w.lane_id==lane]
+    for road_num in road_list[1:]:
+        road_wp = [w for w in carla_map.generate_waypoints(WAYPOINT_DIST)
+                if w.road_id==road_num and w.lane_id==lane]
+        road_wp = [w.transform.location for w in road_wp]
+        last_nav_point = navigation_waypoints[-1]
+        if last_nav_point.distance(road_wp[-1]) < last_nav_point.distance(road_wp[0]):
+            road_wp.reverse()
+        navigation_waypoints = navigation_waypoints + road_wp
+    return navigation_waypoints
+
+def spawn_vehicle(spawn_transform, random_start=True, vehicle_type="audi.tt", vehicle_color="0,65,120"):
+    blueprint = world.get_blueprint_library().find("vehicle."+vehicle_type)
+    blueprint.set_attribute('color', vehicle_color)
+    vehicle_start = spawn_transform
+    vehicle_start.location.z += 0.1 # The waypoint is *below* the road?! This avoids a collision
+    if random_start:
+        vehicle_start.location.x += 3*(np.random.rand()-0.5)
+        vehicle_start.location.y += 3*(np.random.rand()-0.5)
+        original_yaw = vehicle_start.rotation.yaw
+        vehicle_start_rotation = carla.Rotation(yaw=original_yaw + 80*(np.random.rand()-0.5))
+        vehicle_start.rotation = vehicle_start_rotation
+    vehicle = world.spawn_actor(blueprint, vehicle_start)
+    return vehicle
+
+def main():
+    world.tick()
+    vehicles, controllers = [], []
+    num_vehicles = 5
+    spawn_every = 60
+    t = 0
+    while True:
+        try:
+            t += 1
+            for c in controllers:
+                c.update()
+            if t%spawn_every==1 & len(vehicles)<num_vehicles:
+                ego = spawn_vehicle(ego_spawn_transform)
+                controllers.append(pp.PurePursuit(ego, navigation_waypoints, world))
+            world.tick()
+            continue
+        except KeyboardInterrupt:
+            print('\n Destroying all Actors')
+            client.apply_batch([carla.command.DestroyActor(v) for v in vehicles])
+            # carla.command.DestroyActor(ego)
+            client.reload_world()
+            break
+
+if __name__ == "__main__":
+    WAYPOINT_DIST = 1
+    # This list of roads corresponds to the 8-shaped loop
+    TRACK_ROADS = [36, 862, 35, 43, 266, 42, 50, 1174, 49, 902, 48, 775, 47, 1073, 46, 144, 45, 6,
+                       41, 1400, 40]  # , 1185, 39, 1092, 38, 1601, 37, 761]
+    waypoints = [w for w in carla_map.generate_waypoints(WAYPOINT_DIST)
+                 if w.road_id==TRACK_ROADS[0] and w.lane_id == 4]
+    navigation_waypoints = create_location_waypoints(TRACK_ROADS)
+    ego_spawn_transform = waypoints[0].transform
+    # ego = spawn_vehicle(ego_spawn_transform)
+    world.get_spectator().set_transform(ego_spawn_transform)
+    # control = pp.PurePursuit(ego, navigation_waypoints, world)
+    main()
+
